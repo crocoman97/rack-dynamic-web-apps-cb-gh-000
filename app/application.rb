3d882d42d7a00b8087f911034d831f0aed6dd351
Rauof M <abdelrauof197@gmail.com>
2018-02-14 03:14:58 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    number_1 = Kernel.rand(1..2)
+    number_2 = Kernel.rand(1..2)
+    number_3 = Kernel.rand(1..2)
+    resp.write "#{number_1}\n"
+    resp.write "#{number_2}\n"
+    resp.write "#{number_3}\n"
+    if number_1 == number_2 && number_3 == number_2
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
     resp.finish
   end
 
